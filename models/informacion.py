13ae05f37a5b42756897804c73eb4c659c5a9a9b
SXE-2021a <blackhorsef8@gmail.com>
2023-11-15 14:05:32 +0100

    23/11/15

--- models/informacion.py
+++ models/informacion.py
@@ -11,6 +11,12 @@ class informacion(models.Model):
     alto_en_cms = fields.Integer(string="Alto en centímetros:")
     longo_en_cms = fields.Integer(string="Longo en centímetros:")
     ancho_en_cms = fields.Integer(string="Ancho en centímetros:")
+    volume = fields.Float(compute="_volume", store=True)
     peso = fields.Float(digits=(6,2), default =2.7, string="Peso en KG.s:")
     autorizado = fields.Boolean(default = True, string="¿Autorizado?")
     sexo_traducido = fields.Selection([('Hombre','Home'), ('Mujer','Muller'), ('Otros','Outros')], string="Sexo:")
+
+    @api.depends('alto_en_cms', 'longo_en_cms', 'ancho_en_cms')
+    def _volume(self):
+        for rexistro in self:
+            rexistro.volume = float(rexistro.alto_en_cms) * float(rexistro.longo_en_cms) * float(rexistro.ancho_en_cms)
