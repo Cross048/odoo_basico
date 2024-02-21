8d08c78c1f1756342609adb1ccb149579fa6fc8b
SXE-2021a <blackhorsef8@gmail.com>
2024-02-21 14:22:42 +0100

    24/02/21

--- models/informacion.py
+++ models/informacion.py
@@ -1,5 +1,7 @@
 from odoo import models, fields, api
 from odoo.exceptions import ValidationError
+from odoo.exceptions import Warning
+import os
 
 
 class informacion(models.Model):
@@ -60,3 +62,30 @@ class informacion(models.Model):
                 raise ValidationError('Os peso de %s ten que estar entre 1 e 4 ' % rexistro.name)
     def _cambia_campo_sexo(self, rexistro):
         rexistro.sexo_traducido = "Hombre"
+
+    def ver_contexto(self):  # Este método é chamado dende un botón de informacion.xml
+        for rexistro in self:
+            # Ao usar warning temos que importar a libreria mediante from odoo.exceptions import Warning
+            # Importamos tamén a libreria os mediante import os
+            # raise Warning(
+            #     'Contexto: %s Ruta: %s Contido %s' % (rexistro.env.context, os.getcwd(), os.listdir(os.getcwd())))
+            raise ValidationError('Contexto: %s Ruta: %s Contido do directorio %s' % (
+                rexistro.env.context, os.getcwd(), os.listdir(os.getcwd())))
+            # env.context é un diccionario  https://www.w3schools.com/python/python_dictionaries.asp
+        return True
+    def envio_email(self):
+        meu_usuario = self.env.user
+        # mail_de     Odoo pon o email que configuramos en gmail para facer o envio
+        mail_reply_to = meu_usuario.partner_id.email  # o enderezo email que ten asociado o noso usuario
+        mail_para = 'emaildedestino@servidordedestino.com'  # o enderezo email de destino
+        mail_valores = {
+            'subject': 'Aquí iría o asunto do email ',
+            'author_id': meu_usuario.id,
+            'email_from': mail_reply_to,
+            'email_to': mail_para,
+            'message_type': 'email',
+            'body_html': 'Aquí iría o corpo do email cos datos por exemplo de "%s" ' % self.descripcion,
+        }
+        mail_id = self.env['mail.mail'].create(mail_valores)
+        mail_id.sudo().send()
+        return True
