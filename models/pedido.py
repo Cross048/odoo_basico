fb3cd609649c2b87dfa30aac780f3a5e40f024fd
SXE-2021a <blackhorsef8@gmail.com>
2024-02-28 14:01:26 +0100

    24/02/28

--- models/pedido.py
+++ models/pedido.py
@@ -6,6 +6,10 @@ class pedido(models.Model):
     _order = "name asc"
 
     name = fields.Char(required=True, size=20, string="Identificador de Pedido:")
+    persoa_id = fields.Many2one('res.partner', ondelete='set null', domain="[('visible','=','True')]", index=True,
+                                string="Persoa")
+    # persoa_id = fields.Many2one('res.partner', ondelete='set null', domain="[]", index=True,
+    #                             string="Persoa")
 # Os campos One2many Non se almacenan na BD
     lineapedido_ids = fields.One2many("odoo_basico.lineapedido",'pedido_id')
 
