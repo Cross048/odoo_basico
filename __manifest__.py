cfcf44ed5aa6652f5286f0cc511b2f054c21cee2
SXE-2021a <blackhorsef8@gmail.com>
2024-03-12 13:43:50 +0100

    24/03/12

--- __manifest__.py
+++ __manifest__.py
@@ -32,6 +32,7 @@
         'views/templates.xml',
         'reports/report_header.xml',
         'reports/report_informacion.xml',
+        'security/xestion_usuarios.xml',
         'accions_planificadas/accion_planificada.xml',
         'views/menu.xml',
         'security/ir.model.access.csv',
